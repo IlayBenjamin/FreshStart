281efa137f7d47b7ba21973582ed1322aa205138
Ilay Benjamin <91074940+IlayBenjamin@users.noreply.github.com>
2023-12-13 01:29:59 +0200

    Version 10.1
    Added Comments

--- Message.php
+++ Message.php
@@ -10,6 +10,8 @@
             $this->content = (gettype($content) == 'string') ? $content : "";
         }
 
+        //heyyy
+
         public function getMessage() { return $this->content; }
         public function setMessage($content) { $this->content = $content; }
         public function print() {
