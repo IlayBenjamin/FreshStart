f3d3654c9e70702434d961bbcfc0a4d00a58f576
Ilay Benjamin <ilaybenjamin1@gmail.com>
2023-12-12 19:01:32 +0200

    MessageUtilities class

---
+++ MessageUtilites.php
@@ -0,0 +1,15 @@
+<?php
+
+    final class MessageUtilites {
+        static $HELLO_WORLD_MESSAGE_CONTENT = "Hello, World!";
+        static $HEY_MESSAGE_CONTENT = "Hey There!";
+        static $BYE_MESSAGE_CONTENT = "Good Bye!";
+
+        static function PRINT_MESSAGE($message) {
+            print $message;
+        }
+
+    }
+
+
+?>
