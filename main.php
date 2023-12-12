4d3d19b48b2aa0b6bf1b0d35e6e4ed73f372e9bd
Ilay Benjamin <ilaybenjamin1@gmail.com>
2023-12-12 20:33:55 +0200

    v2.12

--- main.php
+++ main.php
@@ -1,8 +1,8 @@
 <?php
 
-    $m = new Message("Version 2.1 Released");
+    $m = new Message("Snapshot 2.12 Released");
     print($m->getMessage());
 
     MessageUtilites::PRINT_EXAMPLE_MESSAGE1();
-
+    MessageUtilites::PRINT_EXAMPLE_MESSAGE3();
 ?>
