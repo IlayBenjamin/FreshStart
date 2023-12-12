ad917254eb1969aa4fada62a6bb6c770526fec41
Ilay Benjamin <ilaybenjamin1@gmail.com>
2023-12-13 00:28:27 +0200

    Version 5.0

--- main.php
+++ main.php
@@ -1,12 +1,13 @@
 <?php
 
-    $m = new Message("Version 4.0 Released");
+    $m = new Message("Version 5.0 Released");
     print($m->getMessage());
 
     $messages = [
         MessageUtilites::$HELLO_WORLD_MESSAGE_CONTENT,
         MessageUtilites::$HEY_MESSAGE_CONTENT,
-        MessageUtilites::$BYE_MESSAGE_CONTENT
+        MessageUtilites::$BYE_MESSAGE_CONTENT,
+        "ILAYYYY"
     ];
 
     MessageUtilites::PRINT_ALL($messages);
