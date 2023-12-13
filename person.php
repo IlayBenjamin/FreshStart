5ff700da923af68747e4483f77a337ef66561fc6
Ilay Benjamin <ilaybenjamin1@gmail.com>
2023-12-13 22:57:16 +0200

    First

---
+++ person.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Person {
+        private string $name;
+        private string $country;
+        public function __construct(?string $name = "", ?string $country = "") {
+            $this->name = (gettype($name) == "string" ? $name : "");
+            $this->country = (gettype($country) == "string" ? $country : "");
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+        public function getCountry() {
+            return $this->country;
+        }
+        
+    }
+
+
+
+?>
