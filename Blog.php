2d3840d67e2e346487feb0d664734a8a95bde508
Ilay Benjamin <ilaybenjamin1@gmail.com>
2023-12-13 22:34:54 +0200

    General Fixes

--- Blog.php
+++ Blog.php
@@ -1,15 +1,25 @@
 <?php
 
   class Blog {
-    private Array $blobContent;
-    private String $owner;
-    private String $bio;
+    private array $blogContent;
+    private string $owner;
+    private string $bio;
 
-    public function __construct(String $ownerName, String $bio) {
-      $this->blobContent = array();
+    public function __construct(string $ownerName, string $bio) {
+      $this->blogContent = array();
       $this->owner = $ownerName;
       $this->bio = $bio;
     }
+
+    public function getOwner() { return $this->owner; }
+    public function getBio() { return $this->bio; }
+    public function getContent() { return $this->blogContent; }
+    public function messageByIndex(int $index) { return $this->blogContent[$index]; }
+    public function setOwner(string $ownerName) { $this->owner = $ownerName; }
+    public function setBio(string $bio) { $this->bio = $bio; }
+    public function loadContent(array $messagesList) { $this->blogContent = $messagesList; }
+    public function uploadMessage(string|array $message) { array_push($this->blogContent, $message); }
+    public function removeMessageByIndex(int $index) { unset($this->blogContent, $index); }
 
   }
 
